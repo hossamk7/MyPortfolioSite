5215789b85916466760a5fc728742a38ccced9a3
Hossam Khedr <hossamk@hotmail.co.uk>
2016-05-23 16:17:43 +0000

    added confirm prompt before deleting comments

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,17 @@
-$("#edit-button").on("click", function(){
-	$("#edit-form").toggleClass("hidden");
-	$("#comment-text").toggleClass("hidden");
+$(document).ready(function(){
+    
+    $("#edit-button").on("click", function(){
+	    $("#edit-form").toggleClass("hidden");
+	    $("#comment-text").toggleClass("hidden");
+    });
+    
+    $("#delete-comment-button").click(function(event){
+        var confirmDel = confirm("Delete comment? Click OK to continue.");
+        if(confirmDel){
+            $('delete-comment-button').submit();
+        } else {
+            event.preventDefault();  
+        }
+    });
+    
 });
