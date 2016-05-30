0074d2704318d3c53495065d1aa00368ec08aeff
Hossam Khedr <hossamk@hotmail.co.uk>
2016-05-30 18:07:40 +0000

    moved index routes to separate module

--- app.js
+++ app.js
@@ -46,63 +46,8 @@ app.use(function(req, res, next){
 
 var pictureRoutes = require("./routes/pictures");
 app.use(pictureRoutes);
-
-// ==========================  ROUTES BEGIN  ==============================
-
-app.get("/pictures", function(req, res){
-    res.render("landing.ejs");
-});
-
-app.get("/signup", function(req, res) {
-   res.render("signup.ejs"); 
-});
-
-app.post("/signup", function(req, res) {
-    var newUser = new User({
-        username: req.body.username,
-        email: req.body.email
-    });
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            return res.render("signup.ejs", {flashRedMessage: err.message });
-        }
-        passport.authenticate("local")(req, res, function(){
-            req.flash("flashGreenMessage", "Hello " + user.username);
-            res.redirect("/pictures/recent");
-        });
-    });
-});
-
-app.post('/login', function(req, res, next){
-    passport.authenticate("local", function(err, user, info){
-        if(err){
-            return next(err);
-        }
-        if (user === false){
-            console.log(info);
-            req.flash("flashRedMessage", info.message);
-            return res.redirect("back");
-        }
-        req.login(user, function(err){
-            if(err){
-                return next(err);
-            }
-            req.flash("flashGreenMessage", "Welcome back " + user.username + "!");
-            return res.redirect("/pictures/recent");        
-        });      
-    })(req, res);
-});
-
-
-app.get("/logout", function(req, res){
-    req.logout();
-    req.flash("flashGreenMessage", "You have been logged out.");
-    res.redirect("/pictures/recent");
-});
-
-
-
+var indexRoutes = require("./routes/index");
+app.use(indexRoutes);
 
 
 // ================ COMMENTS ROUTES =======================
---
+++ routes/index.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router({mergeParams: true});
+var User = require("../models/user");
+var passport = require("passport");
+
+router.get("/", function(req, res){
+    res.render("landing.ejs");
+});
+
+router.get("/signup", function(req, res) {
+   res.render("signup.ejs"); 
+});
+
+router.post("/signup", function(req, res) {
+    var newUser = new User({
+        username: req.body.username,
+        email: req.body.email
+    });
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("signup.ejs", {flashRedMessage: err.message });
+        }
+        passport.authenticate("local")(req, res, function(){
+            req.flash("flashGreenMessage", "Hello " + user.username);
+            res.redirect("/pictures/recent");
+        });
+    });
+});
+
+router.post('/login', function(req, res, next){
+    passport.authenticate("local", function(err, user, info){
+        if(err){
+            return next(err);
+        }
+        if (user === false){
+            console.log(info);
+            req.flash("flashRedMessage", info.message);
+            return res.redirect("back");
+        }
+        req.login(user, function(err){
+            if(err){
+                return next(err);
+            }
+            req.flash("flashGreenMessage", "Welcome back " + user.username + "!");
+            return res.redirect("/pictures/recent");        
+        });      
+    })(req, res);
+});
+
+
+router.get("/logout", function(req, res){
+    req.logout();
+    req.flash("flashGreenMessage", "You have been logged out.");
+    res.redirect("/pictures/recent");
+});
+
+module.exports = router;
